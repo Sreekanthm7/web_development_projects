4fba3ba3d1c8b9c0c73674bb2ddf29c416b0afbb
Sreekanthm7 <sreekanthm549@gmail.com>
2023-07-04 09:50:39 +0530

    fix: done some changes

--- java/javaquestions/textbooksample/substring/Main.java
+++ java/javaquestions/textbooksample/substring/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         SubStringDemo sDemo = new SubStringDemo("hello", "lo");
         sDemo.setCh('o');
-        System.out.println("first occurence: "+sDemo.firstOccurence());
-        
-        System.out.println("index of sub string s2: "+sDemo.findIndex());
+        System.out.println("first occurence: " + sDemo.firstOccurence());
+
+        System.out.println("index of the sub string s 2 : "+sDemo.findIndex());
 
     }
 }
