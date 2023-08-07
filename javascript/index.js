0eaad89ab1972037ac019ebbf8fb55f7aff0f822
Sreekanthm7 <sreekanthm549@gmail.com>
2023-08-07 13:04:10 +0530

    done some changes in programs

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,7 @@ fruits.map((item) => {
   return item
 })
 
-const animals = ["dogss", "cats", "elephants", "tigers", "cheeta"]
+const animals = ["dogss", "cats", "elephants", "tigers", "cheetas"]
 
 animals.push("donkeys")
 
