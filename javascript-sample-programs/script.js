f91cf0cc4e5545e76f3641ecd128c8088c37505d
Sreekanthm7 <sreekanthm549@gmail.com>
2023-09-05 08:33:26 +0530

    feat: done some array methods

--- javascript-sample-programs/script.js
+++ javascript-sample-programs/script.js
@@ -1,10 +1,10 @@
 // const fruits = ["Banana", "Orange", "Apple", "Mango"]
-// fruits.pop()
-// fruits.push("Kiwi")
-// fruits.shift()
-// fruits.unshift("Lemon")
-// fruits[fruits.length] = "Kiwi"
-// delete fruits[0]
+fruits.pop()
+fruits.push("Kiwi")
+fruits.shift()
+fruits.unshift("Lemon")
+fruits[fruits.length] = "Kiwi"
+delete fruits[0]
 
 // const myGirls = ["Cecilie", "Lone"]
 // const myBoys = ["Emil", "Tobias", "Linus"]
