d0c27c9426af285436ee79f0f40b401c6cc1023f
Sreekanthm7 <sreekanthm549@gmail.com>
2023-09-15 13:19:42 +0530

    feat: wip

--- personal-portfolio/src/components/Home.jsx
+++ personal-portfolio/src/components/Home.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react"
 
 export default function Home() {
   return (
     <div>
-        <h1>HOME</h1>
-        <h2>ABOUT</h2>
-        <h2>News</h2>
-        <h2>Contact</h2>
-        <div>
-          <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, assumenda rerum esse magnam modi beatae consequuntur animi numquam ad placeat maxime alias mollitia obcaecati, illum dolore iusto at? Voluptatem, sint?</h1>
-        </div>
+      <h1>HOME</h1>
+      <h2>ABOUT</h2>
+      <h2>News</h2>
+      <h2>Contact</h2>
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et odio
+          obcaecati pariatur, aspernatur blanditiis quasi sapiente aut
+          asperiores ullam molestiae deleniti assumenda nesciunt voluptatibus
+          fugiat impedit vero inventore dolore autem.
+        </p>
+      </div>
     </div>
-    
   )
 }
-
-
-
