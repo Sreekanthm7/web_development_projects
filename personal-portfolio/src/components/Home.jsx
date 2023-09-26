88b283ae0e607a428c52063a73bfce83b6af6476
Sreekanthm7 <sreekanthm549@gmail.com>
2023-09-26 07:13:52 +0530

    fix: added profile for protfolio

--- personal-portfolio/src/components/Home.jsx
+++ personal-portfolio/src/components/Home.jsx
@@ -3,10 +3,21 @@ import React from "react"
 export default function Home() {
   return (
     <div>
-      <h1>HOME</h1>
-      <h2>ABOUT</h2>
-      <h2>News</h2>
-      <h2>Contact</h2>
+      <div>
+        <h1>HOME</h1>
+        <h2>ABOUT</h2>
+        <h2>News</h2>
+        <h2>Contact</h2>
+      </div>
+      <div>
+        <h1>Profile</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum commodi,
+          ullam rerum necessitatibus expedita deleniti iste ex. Beatae deserunt
+          officiis perspiciatis expedita earum omnis reiciendis! Autem
+          cupiditate quasi perspiciatis hic?
+        </p>
+      </div>
       <div>
         <h2>Skills</h2>
         <ul>
